296ee9e506efe53c27d62c0a0076b760f1a796f5
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2020-08-12 19:35:06 +0800

    Initial commit

--- Expresso/Expresso.Android/MainActivity.cs
+++ Expresso/Expresso.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Platform;
 
 namespace Expresso.Droid
 {
@@ -18,7 +19,7 @@ namespace Expresso.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
+            CachedImageRenderer.Init(true);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
